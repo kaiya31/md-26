b102a7089d2a4fa0977481686ec2436686f2a7f8
kaiya31 <naidookaiya@gmail.com>
2026-08-14 11:34:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,10 +12,7 @@ const gifStages = [
 const noMessages = [
     "No",
     "Are you positive? 🤔",
-    "Pookie please... 🥺",
-    "If you say no, I will be really sad...",
-    "I will be very sad... 😢",
-    "Please??? 💔",
+    "You know you want to.. 🥺",
     "Don't do this to me...",
     "Last chance! 😭",
     "You can't catch me anyway 😜"
