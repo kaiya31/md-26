a7555d6254a3febc8d0c36dd35c42cd79a4d82b9
kaiya31 <naidookaiya@gmail.com>
2026-08-14 16:33:25 +0200

    Update yes-script.js

--- yes-script.js
+++ yes-script.js
@@ -62,4 +62,26 @@ function toggleMusic() {
     }
 }
 
+function launchCardAnimation() {
+  const container = document.querySelector('.container')
+  const suits = ['♥️','♠️','♦️','♣️']
+  const values = ['A','K','Q','J','10']
+
+  for (let i = 0; i < 6; i++) {
+    const card = document.createElement('div')
+    card.className = 'poker-card'
+    card.textContent = values[i % values.length] + suits[i % suits.length]
+    container.appendChild(card)
+
+    setTimeout(() => {
+      card.style.transform = `rotate(${i * 15 - 45}deg) translateY(-180px)`
+      card.style.opacity = '1'
+    }, 120 * i)
+
+    setTimeout(() => {
+      card.remove()
+    }, 4500)
+  }
+}
+
 
